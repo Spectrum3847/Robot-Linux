2c44f4268d6be3a11402144b651925743e373b40
Matthew Skolaut <tech2077@gmail.com>
2012-06-25 02:26:10 -0500

    First Commit

---
+++ cam-server.py
@@ -0,0 +1,225 @@
+from cv2 import *
+import math
+import time
+import sys
+import threading
+import SocketServer
+import signal
+import socket
+from decimal import *
+
+t0 = time.clock() ##timer
+
+"""
+SET UP / INSTANTIATION
+"""
+pixels = [(0,0), (0,0), (0,0), (0,0)]
+pixel_data = ""
+#redpill_or_bluepill = 1
+ON_BONE = 0
+capture = None
+writer = None
+
+def returnCAMmessage():
+		
+	global pixels
+
+	return		"\n: " + str(pixels[0][0]) + \
+				"\n: " + str(pixels[0][1]) + \
+				"\n: " + str(pixels[1][0]) + \
+				"\n: " + str(pixels[1][1]) + \
+				"\n: " + str(pixels[2][0]) + \
+				"\n: " + str(pixels[2][1]) + \
+				"\n: " + str(pixels[3][0]) + \
+				"\n: " + str(pixels[3][1])
+
+
+class EchoRequestHandler(SocketServer.BaseRequestHandler ):
+	def setup(self):
+		 print self.client_address, 'connected!'
+
+	def handle(self):
+		global pixel_data
+		#global redpill_or_bluepill
+		data = 'dummy'
+		while data:
+			if data:
+				self.request.sendall(pixel_data)
+				#if not (str(data).find("A")):
+				#redpill_or_bluepill = "A"
+				#elif not (str(data).find("B")):
+				#redpill_or_bluepill = "B"
+		data = self.request.recv(8)
+		print data
+
+	def finish(self):
+		 print self.client_address, 'disconnected!'
+
+
+def sig_handle(signal,frame):
+	print 'Exiting'
+	del(capture)
+	del(writer)
+	sys.exit(0)
+
+
+def main(*args):
+
+	###### ORIGINAL SERVER MAIN FUNCTION START
+	global pixels
+	global pixel_data
+	global ON_BONE
+	global capture
+	global writer
+	#global redpill_or_bluepill
+
+	server = SocketServer.ThreadingTCPServer(('', 8882), EchoRequestHandler)
+	signal.signal(signal.SIGINT, sig_handle)
+	server_thread = threading.Thread(target=server.serve_forever)
+
+	server_thread.daemon = True
+	server_thread.start()
+	###### ORIGINAL SERVER MAIN FUNCTION NON-LOOP END
+	
+	print "caputure started"
+	
+	'''
+	slow network video capture
+	works, but delays code ~30-120sec on all platforms, depending on it's temperment
+	this is due to the implementation in ffmpeg, and is not a part of OpenCV, just a
+	lower level video library used by OpenCV
+	possible workaround by using command line utilities to bind to file stream
+	'''
+	if ON_BONE:
+		capture = cv.CaptureFromFile("http://10.38.47.11/mjpg/video.mjpg?resolution=640x480&.mjpg")
+	else:
+		capture = cv.CaptureFromFile("video.asf")
+	print "capture done"
+
+	if ON_BONE:
+		# same as 'echo 1 > /sys/devices/platform/leds-gpio/leds/beaglebone::usr2/brightness'
+		open('/sys/devices/platform/leds-gpio/leds/beaglebone::usr2/brightness', 'wb').write("1") 
+
+	if capture is None:
+		print "Capture Error"
+		sys.exit()
+
+	height = 480
+	width = 640
+
+	fourcc = cv.CV_FOURCC('M', 'J', 'P', 'G')
+	fps = 10 
+	writer = cv.CreateVideoWriter('compression4.avi', 0, fps, (width, height), 1)
+	if writer is None:
+		print 'Writer Error'
+
+	##		easier to type (not necessarily required)
+	depth1 = cv.CV_8UC1
+	depth3 = cv.CV_8UC3
+
+	##create some of the images which will be used during processing but which can ultimately be disposed
+	hsvImage = cv.CreateMat(height,width, depth3)
+	filteredImage = cv.CreateMat(height,width, depth3)
+	binImage = cv.CreateMat(height,width, depth1)
+
+	##individual HSV channels (all with a depth of 1)
+	hue = cv.CreateMat(height, width, depth1)
+	sat = cv.CreateMat(height, width, depth1)
+	val = cv.CreateMat(height, width, depth1)
+
+	##just some global colors for ease of use
+	r =  (0, 0, 255, 0);
+	g =  (0, 255, 0, 0);
+	w = cv.RGB(255,255,255)
+	b = cv.RGB(0,0,0)
+
+	while True:
+		# print redpill_or_bluepill
+
+		rawImage = cv.QueryFrame(capture)
+		if rawImage is None:
+			break
+
+		"""
+		ACTUAL FILTERING BEGINS HERE
+		"""
+
+		##HSV provides much better initial filtering than RGB (at least for our color LEDs)
+		cv.CvtColor(rawImage, hsvImage, cv.CV_BGR2HSV)
+
+		##split the channels up so we can filter them individually
+		cv.Split(hsvImage, hue, sat, val, None)
+
+		upperhueval = 90
+		lowerhueval = 30
+		uppersatval = 255
+		lowersatval = 150
+		uppervalval = 255
+		lowervalval = 100
+		dilateval = 2
+		approxval = 35
+
+		##only keeps values in respective channels that are within ranges specified
+		cv.InRangeS(hue, lowerhueval, upperhueval, hue) ## red 80-240 #hue 65-100
+		cv.InRangeS(sat, lowersatval, uppersatval, sat)## green 200-255 #sat 30-200
+		cv.InRangeS(val, lowervalval, uppervalval, val)## blue 200-255 #val 200-255
+
+		##only keeps the values that fit all of the previous filters (i.e. the retroreflective tape (hopefully))
+		cv.And(hue,sat, binImage)
+		cv.And(val,binImage,binImage)
+
+
+		'''
+		This is a critical step:
+		If the dilation/erosion or whatever ends up being implemented fails, then everything fails.
+			
+		Purpose:
+		Serves to eliminate the noise caused by the rim in front of the retroreflective tape (on the 3rd tier basket)
+		'''
+		cv.Dilate(binImage, binImage, None, dilateval) 
+
+		##Finds contours (like finding edges/sides)
+		storage = cv.CreateMemStorage(0)
+		contours = cv.FindContours(binImage, storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_TC89_KCOS)
+			
+
+		##arrays which will hold the good/bad polygons
+		squares = []
+		badPolys = []
+
+		while (contours != None) and (len(contours) > 0):
+			hull = cv.ConvexHull2(contours, storage, cv.CV_CLOCKWISE, 1)
+
+			##maybe try different filters?
+			p = cv.ApproxPoly(hull, storage, cv.CV_POLY_APPROX_DP, approxval, 0)
+		
+			## A (good) square must: 1. Be convex, 2. Have four vertices, 3. Have a large area
+			if (cv.CheckContourConvexity(p) != 0) and (len(p) == 4) and (cv.ContourArea(p) >= 5000):
+				squares.append(p)
+			else:
+				badPolys.append(p)
+
+			contours = contours.h_next()
+
+			##TODO: possibly add a filter for polygons within other polygons, but I think findContours accounts for that
+
+		for s in squares:
+			if len(squares) > 0:
+				pixels = s
+			else:
+				pixels = [(0,0), (0,0), (0,0), (0,0)]
+
+		pixel_data = returnCAMmessage()
+		
+		"""
+		END FILTERING
+		"""
+
+	del(capture)
+	del(writer)
+
+	t = time.clock()
+	print t - t0 ,'seconds ellapsed'
+	
+if __name__ == '__main__':
+	main(*sys.argv)
---
+++ imu-server.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+'''
+A TCP ulitity server that allows for a constant output
+serial device to be listened to over TCP without 
+'''
+
+import sys
+import threading
+import serial
+import SocketServer
+import signal
+import socket
+import time
+from decimal import *
+
+WANT_OTHER_MANGOS = 0
+WANT_MOUNTAINS = 0
+ON_BONE = 0
+ON_PI = 0
+VERBOSE = 0
+
+if ON_BONE:
+	SERPORT = '/dev/ttyO2'
+	RX_MUX = 'spi0_sclk'
+	TX_MUX = 'spi0_d0'
+elif ON_PI:
+	SERPORT = '/dev/ttyAMC0'
+else:
+	SERPORT = '/dev/ttyUSB0'
+MUX_MODE = 1
+BAUDRATE = 115200
+TIMEOUT = 2 # serial port timeout is 2 seconds
+RECEIVE_ENABLE = 32
+ser = None 
+serial_data = None
+
+# Initialize hardware serial on beaglebone
+def initializeSerial():
+	global ser
+	global ON_BONE
+	global ON_PI
+
+	if ON_BONE:
+		# set the RX pin for Mode 0 with receive bit on
+		# - use %X formatter, since value written must be in hex (e.g. write "21" for mode 1 with receive enabled = 33)
+		open('/sys/kernel/debug/omap_mux/' + RX_MUX, 'wb').write("%X" % (RECEIVE_ENABLE + MUX_MODE))
+		# set the TX pin for Mode 0
+		open('/sys/kernel/debug/omap_mux/' + TX_MUX, 'wb').write("%X" % MUX_MODE)
+	ser = serial.Serial(SERPORT, BAUDRATE, timeout=TIMEOUT)
+	time.sleep(1)
+	print ser.readline()
+	print ser.readline()
+	print ser.readline()
+	print ser.readline()
+	print ser.readline()
+	if ON_BONE:
+		open('/sys/devices/platform/leds-gpio/leds/beaglebone::usr2/brightness', 'wb').write("1") # echo 1 > /sys/devices/platform/leds-gpio/leds/beaglebone::usr2/brightness 
+
+def returnIMUmessage():
+	global WANT_MOUNTAINS
+	global WANT_OTHER_MANGOS
+	global VERBOSE
+
+	line = ser.readline() # Get line of data
+	try:	
+		roll = line.split("!")[1].split(':')[1].split(',')[1]
+	except IndexError:
+		print "Problem with Serial Port"
+		return "IndexError"
+	pitch = line.split("!")[1].split(':')[1].split(',')[2]
+	yaw = line.split("!")[1].split(':')[1].split(',')[3]
+	accel_x = line.split("!")[1].split(':,')[2].split(',')[0]
+	accel_y = line.split("!")[1].split(':,')[2].split(',')[1]
+	accel_z = line.split("!")[1].split(':,')[2].split(',')[2]
+
+	gyro_x = line.split("!")[1].split(':,')[2].split(',')[3]
+	gyro_y = line.split("!")[1].split(':,')[2].split(',')[4]
+	gyro_z = line.split("!")[1].split(':,')[2].split(',')[5]
+
+	if want_other_mangos:
+		magno_x = line.split("!")[1].split(':,')[2].split(',')[6]
+		magno_y = line.split("!")[1].split(':,')[2].split(',')[7]
+		magno_z = line.split("!")[1].split(':,')[2].split(',')[8]
+	mango_h = line.split("!")[1].split(':,')[2].split(',')[9]
+
+	if want_mountains:
+		baro_t = (float(Decimal(line.split("!")[1].split(':,')[2].split(',')[10]))/10)
+		baro_p = line.split("!")[1].split(':,')[2].split(',')[11]
+
+	if not verbose:
+		return 		"\n: " + roll + \
+					"\n: " + pitch + \
+					"\n: " + yaw + \
+					"\n: " + accel_x + \
+					"\n: " + accel_y + \
+					"\n: " + accel_z + \
+					"\n: " + gyro_x + \
+					"\n: " + gyro_y + \
+					"\n: " + gyro_z + \
+					"\n: " + mango_h
+
+	else:
+		return		"\nRoll: " + roll + \
+	   				"\nPitch: " + pitch + \
+	   				"\nYaw: " + yaw + \
+					"\nAccel x: " + accel_x + \
+					"\nAccel y: " + accel_y + \
+					"\nAccel z: " + accel_z + \
+					"\nGyro x: " + gyro_x + \
+					"\nGyro y: " + gyro_y + \
+					"\nGyro z: " + gyro_z + \
+					"\nMango heading: " + mango_h + \
+					""
+					#"\nMagno x: " + magno_x + \
+					#"\nMagno y: " + magno_y + \
+					#"\nMagno z: " + magno_z + \
+					#"\nTemp C: " + str(baro_t) + \
+					#"\nPressure: " + baro_p +\
+
+
+class EchoRequestHandler(SocketServer.BaseRequestHandler ):
+	'''
+	Due to timing differences in serial communication and TCP,
+	wait conditions occur, or other bugs due to TCP waiting for
+	serial to finish communication, or as found, serial buffer
+	problems that occur from not constantly emptying the serial
+	buffer, leading to old data being sent, or TCP polling an
+	empty buffer from exausting the buffer and waiting for 
+	serial to finish. To fix this, a threaded TCP buffer that
+	polls on client request was designed, which leads to new
+	data consistanly being sent and no buffer race conditions
+	from occuring.
+	'''
+
+	def setup(self): # Tells us when we have a client has connected
+		print self.client_address, 'connected!'
+
+	def handle(self): # Recieves data 
+		global serial_data
+		data = 'dummy'
+		while data:
+			if data:
+				self.request.sendall(serial_data)
+		data = self.request.recv(8)
+
+	def finish(self): # Tells us when we have a client has disconnected
+		print self.client_address, 'disconnected!'
+
+def sig_handle(signal,frame): #So it responds to ^C
+	print 'Exiting'
+	sys.exit(0)
+
+def main(*args):
+
+	global serial_data
+	initializeSerial()
+	server = SocketServer.ThreadingTCPServer(('', 8881), EchoRequestHandler)
+	signal.signal(signal.SIGINT, sig_handle)
+	server_thread = threading.Thread(target=server.serve_forever)
+
+	server_thread.daemon = True
+	server_thread.start()
+
+	while True:
+		serial_data = returnIMUmessage()
+
+if __name__ == '__main__':
+	main(*sys.argv)
